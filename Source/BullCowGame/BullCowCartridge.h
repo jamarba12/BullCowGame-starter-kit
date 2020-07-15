1f31221ca7c75644de907a78479b9e61f68fb686
jamarba12 <jadamarba96@gmail.com>
2020-07-15 09:47:45 -0500

    Out Parameters

--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -21,6 +21,7 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	TArray<FString> GetValidWords(TArray<FString>) const;
 	TArray<FString> Words;
 	TArray<FString> Isograms;
+	void GetBullCows(const FString& Guess, int32& BullCount, int32& CowCount) const;
 	// Your declarations go below!
 	private:
 	FString HiddenWord;
